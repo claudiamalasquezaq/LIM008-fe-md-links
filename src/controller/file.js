29d6be4d565a6eec83d8ff0aea9261bdb683f555
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-03 10:45:05 -0500

    Implementando la función getLinks, falta la ruta del archivo

--- src/controller/file.js
+++ src/controller/file.js
@@ -1,10 +1,23 @@
 import fs from 'fs';
 
+import jsdom from 'jsdom';
+const { JSDOM } = jsdom;
+
+import showdown from 'showdown';
+const converter = new showdown.Converter();
+
 export const getContent = (routeFile) => {
   const fileContent = fs.readFileSync(routeFile).toString();
   return fileContent;
 };
 
 export const getLinks = (contentFile) => {
-
+  const contentHTML = converter.makeHtml(contentFile);
+  const dom = new JSDOM(contentHTML);
+  const arrayOfTagsA = dom.window.document.querySelectorAll('a');
+  let arrNew = [];
+  arrayOfTagsA.forEach(elem => {
+    arrNew.push({href: elem.href, text: elem.textContent});
+  });
+  return arrNew;
 };
