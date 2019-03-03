1080e26e3cc431757ac5fe69d92451d25cdce8af
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-02 20:18:19 -0500

    Implementando test de la función extraer contenido

--- src/controller/test/file.spec.js
+++ src/controller/test/file.spec.js
@@ -1,7 +1,6 @@
 import { getContent, getLinks } from '../file.js'
 
-const outputContent = 
-`# HOLA`
+const outputContent = `[Haz clic aquí para más información](https://www.bing.com/translator)`
 
 describe('getContent', () => {
   it('Debería ser una función', () => {
@@ -12,11 +11,17 @@ describe('getContent', () => {
   });
 });
 
+const outputLinks = [{
+  href:'https://www.bing.com/translator', 
+  text:'Haz clic aquí para más información', 
+  file:'C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\test-md\\README.md'
+}];
+
 describe('getLinks', () => {
   it('Debería ser una función', () => {
     expect(typeof getLinks).toBe('function');
   });
   it('Debería devolver un array de objetos(href, text, file)',() => {
-    expect(getLinks(outputContent)).toBe([{href:'www.myfirstpage.com', text:'Este es un readme', file:'./src/README.md' }])
+    expect(getLinks(outputContent)).toBe(outputLinks);
   });
 });
