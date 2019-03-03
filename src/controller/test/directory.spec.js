1939933637928055f03888f87529a2ec25904dff
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-02 20:17:34 -0500

    Implementando test para verificar si es directorio o archivo

--- src/controller/test/directory.spec.js
+++ src/controller/test/directory.spec.js
@@ -4,10 +4,13 @@ describe('isFilePath', () => {
   it('Debería ser una función', () => {
     expect(typeof isFilePath).toBe('function');
   });
-  it('Debería retornar true si es ruta de un archivo y false si es de un directorio', () => {
-    expect(isFilePath('C:\\Users\\Lab\\projects\\src')).toBe(false);
+  it('Debería retornar true si es ruta de un archivo', () => {
+  expect(isFilePath('C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\file.spec.js')).toBe(true);
   });
-})
+  it('Debería retornar false si es de un directorio', () => {
+    expect(isFilePath('C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test')).toBe(false);
+  });
+});
 
 describe('searchFilesMd', () => {
   it('Debería ser una función', () => {
