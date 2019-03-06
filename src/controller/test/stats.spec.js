5db68d6f48fde743eb51d585cff10126643dbbd7
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-05 22:49:31 -0500

    Modificando error, cambiando un expect por el toBe

--- src/controller/test/stats.spec.js
+++ src/controller/test/stats.spec.js
@@ -4,7 +4,7 @@ const arrInput = [{file: 'C:\\Users\\Lab\\projects\\src\\README.md', href: 'www.
 const arrOutput = {Total: 3, Unique: 3};
 describe('getStats', () => {
   it('Debería ser una función', () => {
-    expect(typeof getStats).expect('function');
+    expect(typeof getStats).toBe('function');
   });
   it('Debería retornar un array de objeto con las estadísticas', () => {
     expect(getStats(arrInput)).toBe(arrOutput);
