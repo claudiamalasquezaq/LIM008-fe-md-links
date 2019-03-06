45069f7585a8e87e955718a49155cf63762f2b15
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-05 22:44:09 -0500

    Implementando las funciones isValidPath y convertPathToAbsolute

--- src/controller/path.js
+++ src/controller/path.js
@@ -1,7 +1,8 @@
 import path from 'path';
+import fs from 'fs';
 
 export const isValidPath = (route) => {
-  
+  return fs.existsSync(route);  
 };
 
 export const isAbsolutePath = (route) => {
@@ -9,6 +10,6 @@ export const isAbsolutePath = (route) => {
 };
 
 export const convertPathToAbsolute = (route) => {
-  
+  return path.resolve(route);
 };
 
