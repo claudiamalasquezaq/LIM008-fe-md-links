5679cb27033429cf3be183cd4696abdff04a36e9
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-05 22:43:31 -0500

    Implementando al objeto la ruta que entra como parámetro de la función getLinks

--- src/controller/file.js
+++ src/controller/file.js
@@ -7,17 +7,17 @@ import showdown from 'showdown';
 const converter = new showdown.Converter();
 
 export const getContent = (routeFile) => {
-  const fileContent = fs.readFileSync(routeFile).toString();
-  return fileContent;
+  return fs.readFileSync(routeFile).toString();
+  // return fileContent;
 };
 
-export const getLinks = (contentFile) => {
+export const getLinks = (contentFile, routeFile) => {
   const contentHTML = converter.makeHtml(contentFile);
   const dom = new JSDOM(contentHTML);
   const arrayOfTagsA = dom.window.document.querySelectorAll('a');
   let arrNew = [];
   arrayOfTagsA.forEach(elem => {
-    arrNew.push({href: elem.href, text: elem.textContent});
+    arrNew.push({href: elem.href, text: elem.textContent, file: routeFile});
   });
   return arrNew;
 };
