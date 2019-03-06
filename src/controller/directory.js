11d4628308584dfdb39f5d3b00ae5d8dca71af70
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-05 22:42:15 -0500

    Agregando una función que devuelve un array con las rutas del directorio y otra para obtener los archivos md

--- src/controller/directory.js
+++ src/controller/directory.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 
 export const isFilePath = (route) => {
 	const stats = fs.lstatSync(route);
@@ -6,6 +7,22 @@ export const isFilePath = (route) => {
 	return answerStat;
 };
 
-export const searchFilesMd = (route) => {
-
+export const getPathsFromDirectory = route => {
+	let arrPathFiles = [];
+  const readDirectory = fs.readdirSync(route);
+  readDirectory.forEach(file => {
+    const pathFile = path.join(route, file);
+    const stats = fs.statSync(pathFile);
+    if (stats.isDirectory()){
+      arrPathFiles = arrPathFiles.concat(getPathsFromDirectory(pathFile))
+    } else {
+      arrPathFiles.push(pathFile);
+    }
+  })
+  return arrPathFiles;
 };
+
+export const searchFilesMd = arrPaths => 
+  arrPaths.filter((file) => {
+    return path.extname(file) === ".md";
+  });
