06489ea8b27918076b5f14cdb1761e6aff3d9d1c
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-07 14:14:31 -0500

    Implementando la función validateLinks

--- src/controller/validate.js
+++ src/controller/validate.js
@@ -1,3 +1,24 @@
-export const validateLinks = (arrLinks) => {
+const fetch = require('node-fetch');
+
+const validateLinksObj = arrLinks => {
+    let newArr = [];
+    arrLinks.map((element) => {
+      return fetch(element.href)
+      .then((res) => {
+        if(res.status >= 200 && res.status < 400){
+          const newObj = {href: element.href, text: element.text, file: element.file, status: res.status, statusText: res.statusText};
+          newArr.push(newObj);
+          // console.log(newObj);
+        } else {
+          const newObj = {href: element.href, text: element.text, file: element.file, status: res.status, statusText: 'Fail'};
+          newArr.push(newObj);
+          // console.log(newObj);
+        }
+        console.log(newArr);
+      })
+      .catch(err => console.log(err));
+    })
+    Promise.all(arrLinks);
+  };
   
-};
+  validateLinksObj([{href:"https://httpbin.org/status/400", text:"text1", file:"ruta1"}, {href:"https://www.google.com/?hl=es_419", text:"text2", file:"ruta2"}]);
