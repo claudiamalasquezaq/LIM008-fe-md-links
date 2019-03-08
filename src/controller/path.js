476b19a9c8b8f0f42e5383b824fcfeec2ac849bd
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-07 19:57:06 -0500

    Borrando los returns y llaves

--- src/controller/path.js
+++ src/controller/path.js
@@ -1,15 +1,10 @@
 import path from 'path';
 import fs from 'fs';
 
-export const isValidPath = (route) => {
-  return fs.existsSync(route);  
-};
+export const isValidPath = route => fs.existsSync(route);
 
-export const isAbsolutePath = (route) => {
-  return path.isAbsolute(route);
-};
+export const isAbsolutePath = route => path.isAbsolute(route);
 
-export const convertPathToAbsolute = (route) => {
-  return path.resolve(route);
-};
+export const convertPathToAbsolute = route => path.resolve(route);
+
 
