659637ad65c218e775f41c8faf1581e6e8b5c7be
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-07 19:58:20 -0500

    Implementando función para que devuelva el objeto modificado con status

--- src/controller/validate.js
+++ src/controller/validate.js
@@ -1,24 +1,34 @@
 const fetch = require('node-fetch');
 
-const validateLinksObj = arrLinks => {
-    let newArr = [];
-    arrLinks.map((element) => {
-      return fetch(element.href)
+const validateLinks = arrLinks => {
+    let arrPromises = [];
+    const arrNew = arrLinks.map(element => new Promise((resolve, reject) => {
+      fetch(element.href)
       .then((res) => {
         if(res.status >= 200 && res.status < 400){
-          const newObj = {href: element.href, text: element.text, file: element.file, status: res.status, statusText: res.statusText};
-          newArr.push(newObj);
-          // console.log(newObj);
+          const newObj = {
+            href: element.href, 
+            text: element.text, 
+            file: element.file, 
+            status: res.status, 
+            statusText: res.statusText
+          };
+          resolve(newObj);
         } else {
-          const newObj = {href: element.href, text: element.text, file: element.file, status: res.status, statusText: 'Fail'};
-          newArr.push(newObj);
-          // console.log(newObj);
+          const newObj = {
+            href: element.href, 
+            text: element.text, 
+            file: element.file, 
+            status: res.status, 
+            statusText: 'Fail'
+          };
+          resolve(newObj);
         }
-        console.log(newArr);
       })
-      .catch(err => console.log(err));
-    })
-    Promise.all(arrLinks);
+      .catch(err => reject(err));
+    }));
+    Promise.all(arrNew)
+    .then((res) => {
+      arrPromises.push(res);
+    });
   };
-  
-  validateLinksObj([{href:"https://httpbin.org/status/400", text:"text1", file:"ruta1"}, {href:"https://www.google.com/?hl=es_419", text:"text2", file:"ruta2"}]);
