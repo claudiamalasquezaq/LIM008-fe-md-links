511b5f3a315e177241ce413448bf451868a2521d
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-07 19:59:58 -0500

    Modificando las rutas con process.cwd

--- src/controller/test/directory.spec.js
+++ src/controller/test/directory.spec.js
@@ -1,27 +1,32 @@
+import path from 'path';
 import { isFilePath, getPathsFromDirectory,  searchFilesMd } from '../directory.js'
 
+const cwd = process.cwd();
+
+const getPath = route => path.join(cwd, route);
+
 const outputPaths = [
-  'C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\directory-test\\directory1\\directory2\\help.md',
-  'C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\directory-test\\directory1\\directory2\\style.css',
-  'C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\directory-test\\directory1\\main.js',
-  'C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\directory-test\\hola.md',
-  'C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\directory-test\\README.md'];
+  'src\\controller\\test\\directory-test\\directory1\\directory2\\help.md',
+  'src\\controller\\test\\directory-test\\directory1\\directory2\\style.css',
+  'src\\controller\\test\\directory-test\\directory1\\main.js',
+  'src\\controller\\test\\directory-test\\hola.md',
+  'src\\controller\\test\\directory-test\\README.md'].map(route => getPath(route));
 
 const outputFilesMd = [
-  'C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\directory-test\\directory1\\directory2\\help.md',
-  'C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\directory-test\\hola.md',
-  'C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\directory-test\\README.md'
-];
+  'src\\controller\\test\\directory-test\\directory1\\directory2\\help.md',
+  'src\\controller\\test\\directory-test\\hola.md',
+  'src\\controller\\test\\directory-test\\README.md'
+].map(route => getPath(route));
 
 describe('isFilePath', () => {
   it('Debería ser una función', () => {
     expect(typeof isFilePath).toBe('function');
   });
   it('Debería retornar true si es ruta de un archivo', () => {
-  expect(isFilePath('C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\file.spec.js')).toBe(true);
+  expect(isFilePath(getPath('src\\controller\\test\\file.spec.js'))).toBe(true);
   });
   it('Debería retornar false si es de un directorio', () => {
-    expect(isFilePath('C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test')).toBe(false);
+    expect(isFilePath(getPath('src\\controller\\test'))).toBe(false);
   });
 });
 
@@ -30,7 +35,7 @@ describe('getPathsFromDirectory', () => {
     expect(typeof getPathsFromDirectory).toBe('function');
   });
   it('Debería recibir la ruta de un directorio y devolver un array con todas las rutas de sus archivos', () =>{
-  expect(getPathsFromDirectory("C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\src\\controller\\test\\directory-test")).toEqual(outputPaths);
+  expect(getPathsFromDirectory(getPath("src\\controller\\test\\directory-test"))).toEqual(outputPaths);
   });
 });
 
