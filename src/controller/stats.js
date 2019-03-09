45d5ad7d3ee3139726aed60cd711fe87f99a13e7
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-08 21:11:54 -0500

    Implementando la función getStats que devuelve un objeto de total y unique links

--- src/controller/stats.js
+++ src/controller/stats.js
@@ -1,3 +1,9 @@
 export const getStats = (arrLinks) => {
-
+  const totalLinks = arrLinks.length;
+  const uniqueLinks = [...new Set(arrLinks.map(elem => elem.href))].length;
+  const stats = {
+    Total: totalLinks,
+    Unique: uniqueLinks
+  }
+  return stats;
 }
