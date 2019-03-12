d268a4dea4b3d4a5bd0e4255cdb9161ef89217d1
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-12 02:40:10 -0500

    Cambiando test de getStats para las funciones getTotalLinks y getUniqueLinks

--- test/stats.spec.js
+++ test/stats.spec.js
@@ -1,4 +1,4 @@
-import { getStats } from '../src/controller/stats.js';
+import { getTotalLinks, getUniqueLinks } from '../src/controller/stats.js';
 
 const arrInput = [
   {file: 'C:\\Users\\Lab\\projects\\src\\README.md', href: 'www.abc.com', text: 'Visita este sitio'},
@@ -10,11 +10,20 @@ const arrInput = [
 
 const arrOutput = {Total: 5, Unique: 4};
 
-describe('getStats', () => {
+describe('getTotalLinks', () => {
   it('Debería ser una función', () => {
-    expect(typeof getStats).toBe('function');
+    expect(typeof getTotalLinks).toBe('function');
   });
-  it('Debería retornar un array de objeto con las estadísticas', () => {
-    expect(getStats(arrInput)).toEqual(arrOutput);
+  it('Debería retornar un string con el total de links', () => {
+    expect(getTotalLinks(arrInput)).toEqual('Total: 5');
+  });
+});
+
+describe('getUniqueLinks', () => {
+  it('Debería ser una función', () => {
+    expect(typeof getUniqueLinks).toBe('function');
+  });
+  it('Debería retornar un string con el total de links únicos', () => {
+    expect(getUniqueLinks(arrInput)).toEqual('Unique: 4');
   });
 });
