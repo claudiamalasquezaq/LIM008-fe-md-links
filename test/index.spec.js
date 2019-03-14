91604e821bdf4471e556430d4d4227d9d829a8da
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-03-14 10:43:43 -0500

    Modificando las rutas del test para que pasen en cualquier computadora

--- test/index.spec.js
+++ test/index.spec.js
@@ -7,17 +7,17 @@ const cwd = process.cwd();
 const inputPath = path.join(cwd, "test\\directory-test");
 
 const outPutArrLinks = [
-  {"file": "C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\test\\directory-test\\directory1\\directory2\\help.md", "href": "https://www.google.com/?hl=es_419", "text": "Google"}, 
-  {"file": "C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\test\\directory-test\\hola.md", "href": "https://www.facebook.com/", "text": "Hola, sígueme en Facebook"},
-  {"file": "C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\test\\directory-test\\hola.md", "href": "https://www.dropbox.com/esta-pagina-no-existe", "text": "Inicia sesión en Dropbox"},
-  {"file": "C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\test\\directory-test\\README.md", "href": "https://www.bing.com/translator", "text": "Haz clic aquí para más información"}
+  {"file": path.join(cwd, "test\\directory-test\\directory1\\directory2\\help.md"), "href": "https://www.google.com/?hl=es_419", "text": "Google"}, 
+  {"file": path.join(cwd, "test\\directory-test\\hola.md"), "href": "https://www.facebook.com/", "text": "Hola, sígueme en Facebook"},
+  {"file": path.join(cwd, "test\\directory-test\\hola.md"), "href": "https://www.dropbox.com/esta-pagina-no-existe", "text": "Inicia sesión en Dropbox"},
+  {"file": path.join(cwd, "test\\directory-test\\README.md"), "href": "https://www.bing.com/translator", "text": "Haz clic aquí para más información"}
 ];
 
 const outPutValidate = [
-  {"file": "C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\test\\directory-test\\directory1\\directory2\\help.md", "href": "https://www.google.com/?hl=es_419", "status": 200, "statusText": "OK", "text": "Google"}, 
-  {"file": "C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\test\\directory-test\\hola.md", "href": "https://www.facebook.com/", "status": 200, "statusText": "OK", "text": "Hola, sígueme en Facebook"}, 
-  {"file": "C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\test\\directory-test\\hola.md", "href": "https://www.dropbox.com/esta-pagina-no-existe", "status": 404, "statusText": "Fail", "text": "Inicia sesión en Dropbox"}, 
-  {"file": "C:\\Users\\VIDEA\\CMMA\\projects\\LIM008-fe-md-links\\test\\directory-test\\README.md", "href": "https://www.bing.com/translator", "status": 200, "statusText": "OK", "text": "Haz clic aquí para más información"}
+  {"file": path.join(cwd, "test\\directory-test\\directory1\\directory2\\help.md"), "href": "https://www.google.com/?hl=es_419", "status": 200, "statusText": "OK", "text": "Google"}, 
+  {"file": path.join(cwd, "test\\directory-test\\hola.md"), "href": "https://www.facebook.com/", "status": 200, "statusText": "OK", "text": "Hola, sígueme en Facebook"}, 
+  {"file": path.join(cwd, "test\\directory-test\\hola.md"), "href": "https://www.dropbox.com/esta-pagina-no-existe", "status": 404, "statusText": "Fail", "text": "Inicia sesión en Dropbox"}, 
+  {"file": path.join(cwd, "test\\directory-test\\README.md"), "href": "https://www.bing.com/translator", "status": 200, "statusText": "OK", "text": "Haz clic aquí para más información"}
 ];
 
 describe('getArrLinks', () => {
